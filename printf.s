2bd78a3afa8925441ad0cb97a45270521ff009df
Glebovski <andrewglebovski@gmail.com>
2023-03-19 11:55:19 +0300

    Add printf function
    Prints string using arguments from stack

--- printf.s
+++ printf.s
@@ -1,19 +1,47 @@
 global _start
 
+
 section .text
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
-    syscall
 
-    mov eax, 60
-    xor rdi, rdi
-    syscall
+_start:         ; Calling printf
+                push len
+                push msg
+                call printf
+
+                ; Exit 0
+                mov eax, 60
+                xor rdi, rdi
+                syscall
+
+
+;----------------------------------------
+; Prints data using format string
+;
+; Arguments order in stack:
+;  <variable number of arguments in reverse order>
+;  <format string size>;
+;  <format string pointer>
+;----------------------------------------
+; Enter:    None
+; Exit:     None
+; Destr:    RAX, RDX, RDI, RSI
+;----------------------------------------
+
+printf:         ; Syscall to output using arguments from stack
+                mov rax, 1
+                mov rdi, 1
+                mov rsi, [rsp+8]
+                mov rdx, [rsp+16]
+                syscall
+
+                ret
+
+;----------------------------------------
+
 
 section .data
 
-msg     db "Hello, World!", 10
-len     equ $ - msg
+
+msg             db "Hello, World!", 10  ; Message to print
+len             equ $ - msg             ; Message length
